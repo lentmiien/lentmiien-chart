c57cde7b82f9e2e83bbbbc6ff978c3fe02e7fa29
lentmiien <lentmiien@gmail.com>
2019-08-06 23:44:51 +0900

    Prepare to load csv data

--- public/script.js
+++ public/script.js
@@ -1,15 +1,67 @@
-let c = d3.select('#month_graph').selectAll('circle');
+const plotable_col_ids = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+const global_data = {};
 
-c.data([100, 200, 300]);
-let ce = c.enter().append('circle');
+/* Data structure
 
-c.style('fill', 'red');
-ce.style('fill', 'green');
+global_data
+  Year {
+      data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+      array: [
+        2018: {...},
+        2019: {
+            data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+            array: [
+                1: {...},
+                2: {...},
+                 ...
+                11: {...},
+                12: {
+                    data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+                    array: [
+                        1: {...},
+                        2: {...},
+                         ...
+                        30: {...},
+                        31: {
+                            data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+                        }
+                    ]
+                }
+            ]
+        }
+      ]
+  }
 
-c.attr('r', 10);
-ce.attr('r', 15);
-c.attr('cy', 100);
-ce.attr('cy', 100);
-c.attr('cx', (d, i) => {return d;});
-ce.attr('cx', (d, i) => {return d;});
+*/
 
+GetData();
+async function GetData() {
+    // Load data
+    const response = await fetch('data_file.csv');
+    let load_data = await response.text();
+    let rows = load_data.split("\n").slice(5);// Remove all uncomplete rows
+}
+
+function Plot() {
+    const graph = d3.select('#month_graph');
+
+    // Line helper function
+    const lineFunction = d3.line()
+        .x(function (d, i) { return i * 25; })
+        .y(function (d) { return 600 - d; });
+
+    // Draw line
+    graph.append("path")
+        .attr("d", lineFunction(global_data.amonth))
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none");
+    
+    // Draw dots
+    const c = graph.selectAll('circle');
+    let ce = c.data(global_data.amonth).enter().append('circle');
+    ce.style('fill', 'green');
+    ce.attr('r', 5);
+    ce.attr('cy', (d, i) => { return 600 - d; });
+    ce.attr('cx', (d, i) => { return i * 25; });
+}
