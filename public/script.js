0e6666c648433c20d082e39319a49e53894739b3
lentmiien <lentmiien@gmail.com>
2019-08-07 20:10:41 +0900

    weekday

--- public/script.js
+++ public/script.js
@@ -1,15 +1,19 @@
 const plotable_col_ids = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-const global_data = {};
+const global_data = {
+	year: {}
+};
 
 /* Data structure
 
 global_data
-  Year {
+  year: {
       data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+      weekday: { 0: [plotable_col_ids], 1...6: [plotable_col_ids] },
       array: [
         2018: {...},
         2019: {
             data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+            weekday: { 0: [plotable_col_ids], 1...6: [plotable_col_ids] },
             array: [
                 1: {...},
                 2: {...},
@@ -17,6 +21,7 @@ global_data
                 11: {...},
                 12: {
                     data: { total: [plotable_col_ids], days: [plotable_col_ids] },
+                    weekday: { 0: [plotable_col_ids], 1...6: [plotable_col_ids] },
                     array: [
                         1: {...},
                         2: {...},
@@ -40,6 +45,13 @@ async function GetData() {
     const response = await fetch('data_file.csv');
     let load_data = await response.text();
     let rows = load_data.split("\n").slice(5);// Remove all uncomplete rows
+    
+    for (let i = rows.length - 1; i >= 0; i--) {
+    	let year = rows[i][0];
+        let month = rows[i][1];
+        let date = rows[i][2];
+        let wday = (new Date(parseInt(year), parseInt(month) - 1, parseInt(date))).getDay();
+    }
 }
 
 function Plot() {
